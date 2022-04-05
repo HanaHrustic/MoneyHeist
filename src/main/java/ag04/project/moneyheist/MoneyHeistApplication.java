ac0cb66b139fdb9e2ad7a0e5d8a78a405c562dee
Hana Hrustić <hana.hrustic@gmail.com>
2022-04-05 12:23:08 +0200

    Initialize Spring Boot Project with H2 database

---
+++ src/main/java/ag04/project/moneyheist/MoneyHeistApplication.java
@@ -0,0 +1,13 @@
+package ag04.project.moneyheist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHeistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyHeistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ag04/project/moneyheist/MoneyHeistApplicationTests.java
@@ -0,0 +1,13 @@
+package ag04.project.moneyheist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHeistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
